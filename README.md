4022912295c641deae25e4734ddc92a9a24d90fd
papix <mail@papix.net>
2016-04-11 14:35:38 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Acme::NantokaMemories - It's new $module
+
+# SYNOPSIS
+
+    use Acme::NantokaMemories;
+
+# DESCRIPTION
+
+Acme::NantokaMemories is ...
+
+# LICENSE
+
+Copyright (C) papix.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+papix <mail@papix.net>
---
+++ lib/Acme/NantokaMemories.pm
@@ -0,0 +1,39 @@
+package Acme::NantokaMemories;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::NantokaMemories - It's new $module
+
+=head1 SYNOPSIS
+
+    use Acme::NantokaMemories;
+
+=head1 DESCRIPTION
+
+Acme::NantokaMemories is ...
+
+=head1 LICENSE
+
+Copyright (C) papix.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+papix E<lt>mail@papix.netE<gt>
+
+=cut
+
